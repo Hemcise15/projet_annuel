83d73331b5e432623ee1bfdbaf7a6d421c5f72d6
amelbdj <amelboudjenanee@icloud.com>
2025-04-28 22:14:10 +0200

    Newsletter
    Passage en php du back et du front office avec en plus les fichier nexsletter (fichier traitement pour envoie mail bienvenue newsletter potentiellement modif le nom)
    
    Signed-off-by: amelbdj <amelboudjenanee@icloud.com>

---
+++ create_template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Newsletter - HelpMe Out</title>
+</head>
+<body style="font-family: Arial, sans-serif;">
+    <h1>{{titre}}</h1>
+    
+    <p>{{contenu}}</p>
+    <footer>
+        <small>Merci d'utiliser HelpMe Out ❤️</small>
+    </footer>
+</body>
+</html>
